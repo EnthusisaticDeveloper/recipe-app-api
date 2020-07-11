1d4bd28bc57c7fb55ce4767b6405795180846ec5
Shanly Rajan <shanly.rajan@kazang.com>
2020-07-11 18:44:12 +0200

    :art: Added working list tags endpoint

---
+++ app/recipe/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+# default router is a feature of DRF that will automatically generate URLS for
+# our viewset
+
+# api/recipe/tags
+# api/recipe/tags/id
+
+# Default router automatically registers the appropriate URLs for all of the
+# actions in our viewset
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+# this registers our viewset with our router
+
+app_name = 'recipe'  # for reverse function to lookup correct URLs
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
